3b0d5609b604085cf6606d2d123208216097f733
Sphurti More <sphurti008@gmail.com>
2024-09-29 15:01:50 -0700

    renaming file

--- application.py
+++ application.py
@@ -33,7 +33,7 @@ def predict_datapoint():
         return rendere_template('home.html',results=results[0])
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0")
 
 
 
